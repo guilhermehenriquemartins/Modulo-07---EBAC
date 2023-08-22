e559ffb1b26139d879ddc02cf467177bd32192d2
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-08-21 22:45:10 -0300

    2 Passo de editar o Formulário (Aula 04)

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-//COMO ELIMINAR O RELOAD DA PÁGINA AO REALIZAR SUBMIT NO FORMULÁRIO
+const form = window.document.getElementById('form-deposito')
 
-//1º PASSO - Temos que selecionar o formulário
-
-const form = document.getElementById('form-deposito')
-
-//2º PASSO - Temos que alterar o evento da ação submit do formulário
+function validaNome(nomeCompleto) {
+  nomeComoArray = nomeCompleto.split(' ')
+  return nomeComoArray.length >= 2
+}
 
 form.addEventListener('submit', function(e){
-  //nome do elemento.evento que vai ouvir uma ação (nome da ação), ação que vai acontecer após disparar o submit(parâmetro de submit)
   e.preventDefault();
+
+  let nomeBeneficiario = window.document.getElementById('nome-beneficiario')
+  if (validaNome(nomeBeneficiario.value)) {
+    alert('nome incompleto!');
+  } else {
+    alert('nome completo!');
+  }
 })
