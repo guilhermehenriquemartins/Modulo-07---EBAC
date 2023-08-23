00959215e67f1755e721f4dcebf0946bbef5224f
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-08-22 22:36:25 -0300

    Conclusão Aula 4

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 const form = window.document.getElementById('form-deposito')
 
 function validaNome(nomeCompleto) {
-  nomeComoArray = nomeCompleto.split(' ')
-  return nomeComoArray.length >= 2
+  var nomeComoArray = nomeCompleto.split(' ');
+  return nomeComoArray.length >= 2;
 }
 
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', function(e) {
   e.preventDefault();
+  let validaformulario = false
 
   let nomeBeneficiario = window.document.getElementById('nome-beneficiario')
-  if (validaNome(nomeBeneficiario.value)) {
-    alert('nome incompleto!');
+  let NumeroConta = window.document.getElementById('numero-conta')
+  let NumeroValor = window.document.getElementById('numero-valor')
+  let mensagemSucesso = `Montante de ${NumeroValor.value} será transferido ao usuário ${nomeBeneficiario.value} - conta: ${NumeroConta.value}`
+  validaformulario = validaNome(nomeBeneficiario.value)
+  if(validaformulario) {
+    alert(mensagemSucesso)
+
+    nomeBeneficiario.value = '';
+    NumeroConta.value = '';
+    NumeroValor.value = '';
   } else {
-    alert('nome completo!');
+    alert('[ERRO]: Nome do usuário está incompleto')
   }
 })
