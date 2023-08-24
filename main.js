08291187daa730524162bf811a9b1729db8d9f27
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-08-23 22:55:40 -0300

    Início Aula 05

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 const form = window.document.getElementById('form-deposito')
 
 function validaNome(nomeCompleto) {
-  var nomeComoArray = nomeCompleto.split(' ');
-  return nomeComoArray.length >= 2;
+  NomeComoArray = nomeCompleto.split(' ');
+  return NomeComoArray.length >= 2;
 }
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e){
   e.preventDefault();
-  let validaformulario = false
+  let validaForm = false
 
   let nomeBeneficiario = window.document.getElementById('nome-beneficiario')
   let NumeroConta = window.document.getElementById('numero-conta')
   let NumeroValor = window.document.getElementById('numero-valor')
-  let mensagemSucesso = `Montante de ${NumeroValor.value} será transferido ao usuário ${nomeBeneficiario.value} - conta: ${NumeroConta.value}`
-  validaformulario = validaNome(nomeBeneficiario.value)
-  if(validaformulario) {
-    alert(mensagemSucesso)
+  let mensagemSucesso = `Montante de <strong>${NumeroValor.value}</strong> enviado para o beneficiário: <strong>${nomeBeneficiario.value}</strong> - conta: <strong>${NumeroConta.value}</strong>`
+  validaForm = validaNome(nomeBeneficiario.value)
+  if (validaForm) {
+    mensagemContainer= document.querySelector('.mensagem-sucesso')
+    mensagemContainer.innerHTML = mensagemSucesso
+    mensagemContainer.style.display = 'block'
 
-    nomeBeneficiario.value = '';
-    NumeroConta.value = '';
-    NumeroValor.value = '';
+    nomeBeneficiario.value = ''
+    NumeroConta.value = ''
+    NumeroValor.value = ''
   } else {
-    alert('[ERRO]: Nome do usuário está incompleto')
+    alert('[ERRO]: O campo nome está incompleto, tente novamente.')
   }
 })
