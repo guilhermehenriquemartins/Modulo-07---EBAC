f1afa9bf72c34075fc8d3132dcca6473323ef9a5
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-08-24 21:59:42 -0300

    Parte 2 - Aula 05

--- main.js
+++ main.js
@@ -13,16 +13,21 @@ form.addEventListener('submit', function(e){
   let NumeroConta = window.document.getElementById('numero-conta')
   let NumeroValor = window.document.getElementById('numero-valor')
   let mensagemSucesso = `Montante de <strong>${NumeroValor.value}</strong> enviado para o beneficiário: <strong>${nomeBeneficiario.value}</strong> - conta: <strong>${NumeroConta.value}</strong>`
+  let msg = window.document.querySelector('.msg-sucesso')
   validaForm = validaNome(nomeBeneficiario.value)
+  let msg_erro = window.document.querySelector('.error-message')
   if (validaForm) {
-    mensagemContainer= document.querySelector('.mensagem-sucesso')
-    mensagemContainer.innerHTML = mensagemSucesso
-    mensagemContainer.style.display = 'block'
+    msg.innerHTML = mensagemSucesso
+    msg.style.display = 'block'
+    msg_erro.style.display = 'none'
+    
 
     nomeBeneficiario.value = ''
     NumeroConta.value = ''
     NumeroValor.value = ''
   } else {
-    alert('[ERRO]: O campo nome está incompleto, tente novamente.')
+    nomeBeneficiario.style.border = '1px solid red'
+    msg_erro.style.display = 'block'
+    msg.style.display = 'none'
   }
 })
