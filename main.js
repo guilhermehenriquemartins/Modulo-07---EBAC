061337cbdb2210bde5a5e28d628c9461748c69b2
Guilherme Henrique <88465375+guilhermehenriquemartins@users.noreply.github.com>
2023-09-02 10:18:03 -0300

    Revisão Aulas

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
 const form = window.document.getElementById('form-deposito')
-let validaFormulario = false
-let nomeBeneficiario = window.document.getElementById('nome-beneficiario')
+let validaForm = false;
+nomeBeneficiario = window.document.getElementById('nome-beneficiario')
 
 function validaNome(nomeCompleto) {
   NomeComoArray = nomeCompleto.split(' ');
   return NomeComoArray.length >= 2;
 }
 
-form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e){
   e.preventDefault();
-
-  let numeroConta = window.document.getElementById('numero-conta')
-  let valorDeposito = window.document.getElementById('numero-valor')
-  let mensagemSucesso = `Valor <strong>${valorDeposito.value}</strong> depositado para o beneficiário <strong>${nomeBeneficiario.value}</strong> - conta: <strong>${numeroConta.value}</strong>`
-  validaFormulario = validaNome(nomeBeneficiario.value)
-  if (validaFormulario) {
+  
+  numeroConta = window.document.getElementById('numero-conta')
+  numeroValor = window.document.getElementById('numero-valor')
+  mensagemSucesso = `Valor de <strong>${numeroValor.value}</strong> depositado para o cliente <strong>${nomeBeneficiario.value}</strong> - conta: <strong>${numeroConta.value}</strong>`
+  validaForm = validaNome(nomeBeneficiario.value)
+  if (validaForm) {
     window.document.querySelector('.resultado').innerHTML = mensagemSucesso
     window.document.querySelector('.resultado').style.display = 'block'
-    mensagemErro.style.display = 'none'
   } else {
-    mensagemErro = window.document.querySelector('.mensagem-erro')
-    window.document.querySelector('.resultado').style.display = 'none'
-    mensagemErro.style.display = 'block'
+    window.document.querySelector('.erro-beneficiario').style.display = 'block'
     nomeBeneficiario.style.border = '1px solid red'
   }
 })
 
 nomeBeneficiario.addEventListener('keyup', function(e){
-  validaFormulario = validaNome(e.target.value)
-
-  if (!validaFormulario) {
+  validaForm = validaNome(e.target.value)
+  if (!validaForm) {
     nomeBeneficiario.classList.add('error')
-    window.document.querySelector('.mensagem-erro').style.display = 'block'
+    window.document.querySelector('.erro-beneficiario').style.display = 'block'
     //nomeBeneficiario.style.border = '1px solid red'
   } else {
-    mensagemErro = window.document.querySelector('.mensagem-erro')
-    mensagemErro.style.display = 'none'
-    window.document.querySelector('.resultado').style.display = 'block'
     nomeBeneficiario.classList.remove('error')
+    window.document.querySelector('.erro-beneficiario').style.display = 'none'
+    //nomeBeneficiario.style = ''
   }
 })
+
